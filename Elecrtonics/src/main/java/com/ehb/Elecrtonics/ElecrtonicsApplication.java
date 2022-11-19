bf14630942f82712c74e70971a7caefae860a01d
yassin <yassin.kallouch@student.ehb.be>
2022-11-19 14:17:54 +0100

    Project setup using java(spring)

---
+++ Elecrtonics/src/main/java/com/ehb/Elecrtonics/ElecrtonicsApplication.java
@@ -0,0 +1,13 @@
+package com.ehb.Elecrtonics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElecrtonicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElecrtonicsApplication.class, args);
+	}
+
+}
---
+++ Elecrtonics/src/test/java/com/ehb/Elecrtonics/ElecrtonicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehb.Elecrtonics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElecrtonicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
