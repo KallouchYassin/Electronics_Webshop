88304bca0063ad4e2d0d30d77f46651e65a4195a
yassin <yassin.kallouch@student.ehb.be>
2022-12-13 22:41:39 +0100

    seeding table

--- Elecrtonics/src/main/resources/data.sql
+++ Elecrtonics/src/main/resources/data.sql
@@ -2,14 +2,14 @@ INSERT INTO `category` (`id`, `cat_nam`, `description`, `url`) VALUES (NULL, 'FR
 INSERT INTO `category` (`id`, `cat_nam`, `description`, `url`) VALUES (NULL, 'RADIATOR', 'RADIATOR', 'https://i.postimg.cc/zfsKsVTT/ch.jpg');
 INSERT INTO `category` (`id`, `cat_nam`, `description`, `url`) VALUES (NULL, 'coffee maker', 'coffee maker', 'https://i.postimg.cc/SxQrd2Gs/cafe2.jpg');
 
-INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'Fridge1', 'https://i.postimg.cc/JhkB5MS0/frigo.webp', 'fridge1', '102.21', '1');
-INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'Fridge2', 'https://i.postimg.cc/JhkB5MS0/frigo.webp', 'fridge2', '10.21', '1');
+INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'Fridge1', 'https://i.postimg.cc/j52mF3QD/frigo2jfif.jpg', 'fridge1', '102.21', '1');
+INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'Fridge2', 'https://i.postimg.cc/fbpfMHQt/frigo6.jpg', 'fridge2', '10.21', '1');
 INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'Fridge3', 'https://i.postimg.cc/JhkB5MS0/frigo.webp', 'fridge3', '5602.21', '1');
 
 INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'micro1', 'https://i.postimg.cc/JhkB5MS0/frigo.webp', 'fridge1', '1981', '2');
-INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'micro2', 'https://i.postimg.cc/JhkB5MS0/frigo.webp', 'fridge1', '354.52', '2');
-INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'micro3', 'https://i.postimg.cc/JhkB5MS0/frigo.webp', 'fridge1', '21548.21', '2');
+INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'micro2', 'https://i.postimg.cc/nh8xQxK0/micro2jfif.jpg', 'fridge1', '354.52', '2');
+INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'micro3', 'https://i.postimg.cc/5yZhv5z6/ch9.jpg', 'fridge1', '21548.21', '2');
 
-INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'cafe1', 'https://i.postimg.cc/JhkB5MS0/frigo.webp', 'fridge1', '0325.21', '3');
-INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'cafe2', 'https://i.postimg.cc/JhkB5MS0/frigo.webp', 'fridge1', '8965.21', '3');
-INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'cafe3', 'https://i.postimg.cc/JhkB5MS0/frigo.webp', 'fridge1', '856.21', '3');
+INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'cafe1', 'https://i.postimg.cc/T1g6tHfr/cafe.jpg', 'fridge1', '0325.21', '3');
+INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'cafe2', 'https://i.postimg.cc/yYjhD6zz/micro5.jpg', 'fridge1', '8965.21', '3');
+INSERT INTO `products` (`id`, `description`, `image_url`, `name`, `price`, `category_id`) VALUES ('', 'cafe3', 'https://i.postimg.cc/3r4Jyt5r/ch3.jpg', 'fridge1', '856.21', '3');
